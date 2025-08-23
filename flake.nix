a85709fb41c9db0ea61358ff5f5f60b1c036a3dd
Lasse H. Bomholt <lasse@bomh.net>
2025-08-23 13:28:25 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "Replace me";
+
+  inputs = {
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, fenix, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          f = with fenix.packages.${system}; combine [
+            stable.toolchain
+            targets.wasm32-unknown-unknown.stable.rust-std
+          ];
+        in
+          {
+            devShells.default = 
+              pkgs.mkShell {
+                name = "replace-me";
+
+                systemPackages = with pkgs; [
+                  rust-analyzer
+                ];
+
+                buildInputs = with pkgs; [
+                  bashInteractive
+                  nodePackages.pnpm
+                  nodejs_20
+                ];
+                
+                packages = with pkgs; [
+                  f
+                  linuxPackages_latest.perf
+                  lldb
+                  llvmPackages.bintools
+                  nodePackages.typescript-language-server
+                  nodejs_21
+                  vscode-langservers-extracted
+                  wasm-pack
+                ];
+
+                CARGO_TARGET_WASM32_UNKNOWN_UNKNOWN_LINKER = "lld";
+              };
+          }
+      );
+}
